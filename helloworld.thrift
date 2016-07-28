c95e170bf37fe569f85c4072d9c000b0250507f9
Souradip Guha <souradip.guha@yahoo.com>
2016-07-28 15:32:43 +0530

    Update helloworld.thrift

--- helloworld.thrift
+++ helloworld.thrift
@@ -1,9 +1,9 @@
-const string HELLO_IN_KOREAN = "an-nyoung-ha-se-yo"
-const string HELLO_IN_FRENCH = "bonjour!"
-const string HELLO_IN_JAPANESE = "konichiwa!"
+const string HELLO_IN_ENGLISH = "HELLO"
+const string HELLO_IN_BENGALI = "NOMOSKAR"
+const string HELLO_IN_HINDI = "NAMASKAR"
  
 service HelloWorld {
   void ping(),
-  i32 sayHello(),
-  i32 sayMsg(1:string msg)
+  string sayHello(),
+  string sayMsg(1:string msg)
 }
